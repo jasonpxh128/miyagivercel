9526fed9451703bc4943c2cde1e591f13d146a87
Jason Pua <jasonpua@Jasons-MacBook-Air.local>
2023-02-18 18:05:21 +0800

    changed nextjs Image to html img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,8 +19,8 @@ export default function hero({}: Props) {
   return (
     <div className='h-screen flex flex-col items-center justify-center text-center overflow-hidden'> 
         {/*<BackgroundCircles />*/}
-            <Image className="relative rounded-full h-40 w-40 mx-auto object-cover p-10" src= {circlelogo}/>
-            {/* <img className=' relative rounded-full h-40 w-40 mx-auto object-cover mb-8' src="/assets/logo.jpg" alt="" /> */}
+            {/* <Image className="relative rounded-full h-40 w-40 mx-auto object-cover p-10" src= {circlelogo}/> */}
+            <img className=' relative rounded-full h-40 w-40 mx-auto object-cover mb-8' src="/assets/logo.jpg" alt="" />
             <h2 className='text-sm uppercase Utext-gray-500 pb-2 tracking-[15px]'>Detailing Geeks</h2>
             <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
                 <span>{text}</span>
